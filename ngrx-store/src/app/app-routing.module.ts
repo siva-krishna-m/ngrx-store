6e634b0bc070508bbd9bbef26e1db24496b6ea05
sivakrishna <sivakrishnamadasu1996@gmail.com>
2022-05-07 19:26:33 +0530

    dev tools implementation

--- ngrx-store/src/app/app-routing.module.ts
+++ ngrx-store/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { CounterComponent } from './counter/counter/counter.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "counter", component: CounterComponent },
+  { path: "posts", component: PostsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ngrx-store/src/app/app.module.ts
+++ ngrx-store/src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { environment } from './../environments/environment';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { HeaderComponent } from './shared/header/header.component';
 import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
 import { counterReducer } from './counter/state/counter.reducer';
 import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
@@ -10,6 +13,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -17,12 +22,18 @@ import { FormsModule } from '@angular/forms';
     CounterComponent,
     CounterButtonsComponent,
     CounterOutputComponent,
-    CustomCounterInputComponent
-  ],
+    CustomCounterInputComponent,
+    HomeComponent,
+    HeaderComponent,
+    PostsListComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({ counter: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      logOnly: environment.production
+    }),
     FormsModule
   ],
   providers: [],
